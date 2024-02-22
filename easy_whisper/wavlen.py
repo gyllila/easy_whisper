f56641b549ada83f1c92469473116f62367f2970
Yanling Guo <74056988+gyllila@users.noreply.github.com>
2024-02-22 01:45:56 +0100

    Delete easy_whisper/wavlen.py

--- easy_whisper/wavlen.py
+++
@@ -1,9 +0,0 @@
-import wave
-import contextlib
-
-def wav_length(fname):
-    with contextlib.closing(wave.open(fname,'r')) as f:
-        frames = f.getnframes()
-        rate = f.getframerate()
-        duration = frames / float(rate)
-        return duration
