d93a009669783e037ebfae180cdf154e471dd1b5
Yanling Guo <74056988+gyllila@users.noreply.github.com>
2024-02-22 01:47:14 +0100

    Update __init__.py

--- easy_whisper/__init__.py
+++ easy_whisper/__init__.py
@@ -1,11 +1,10 @@
 __app_name__ = "easy_whisper"
-__version__ = "1.0.0"       
+__version__ = "1.1.0"       
 __all__ = [
         '__main__',
         'ui_share',
         'cli_ui',
         'tk_ui',
         'trans',
-        'sabs',
-        'wavlen'
+        'api_trans'
         ]
